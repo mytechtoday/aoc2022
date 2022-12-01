55c8ca3d3ab16f3995833fb603e01e27cabb0490
Bishow Paudel <bishow.paudel@gmail.com>
2022-12-01 01:45:08 -0600

    advent of code 2022 Solution Day 1 Kotlin

---
+++ src/main/kotlin/mytechtoday/aoc/Day01.kt
@@ -0,0 +1,32 @@
+package mytechtoday.aoc
+
+fun main() {
+    fun getCalList(input: List<String>): MutableList<Int> {
+        var sum: Int = 0;
+        val subTotal = mutableListOf<Int>()
+        for (item in input) {
+            if (item.isNotBlank()) {
+                sum += Integer.valueOf(item);
+            } else {
+                subTotal.add(sum)
+                sum =0;
+            }
+        }
+        return subTotal
+    }
+
+    fun part1(input: List<String>): Int {
+        var subTotals = getCalList(input)
+        return subTotals.max();
+    }
+
+    fun part2(input: List<String>): Int {
+        var subTotals = getCalList(input)
+        var sorted = subTotals.sortedDescending();
+        return sorted[0] + sorted[1] + sorted[2];
+    }
+
+    val input = readInput("22Day01")
+    println(part1(input))
+    println(part2(input))
+}
--- src/main/kotlin/mytechtoday/aoc/Utils.kt
+++ src/main/kotlin/mytechtoday/aoc/Utils.kt
@@ -1,15 +1,18 @@
+package mytechtoday.aoc
+
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.Objects
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+//fun readInput(fileName: String) = this::class.java.getResourceAsStream(fileName).bufferedReader().readLines()
+fun readInput(name: String) = File("src/main/resources", "$name.txt")
     .readLines()
-
 /**
- * Converts string to md5 hash.
+ * Converts string to mytechtoday.aoc.md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
